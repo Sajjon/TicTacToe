955be185cf55b497cea9f96157aff4f147139eb2
Alexander Cyon <alex.cyon@gmail.com>
2020-04-08 21:52:41 +0200

    First working ascii version of tictactoe

---
+++ Sources/TicTacToe/ReadInteger.swift
@@ -0,0 +1,28 @@
+
+protocol IntegerFromString: BinaryInteger {
+    init?(_ description: String)
+}
+extension Int: IntegerFromString {}
+extension UInt: IntegerFromString {}
+extension Int8: IntegerFromString {}
+extension UInt8: IntegerFromString {}
+extension Int16: IntegerFromString {}
+extension UInt16: IntegerFromString {}
+extension Int32: IntegerFromString {}
+extension UInt32: IntegerFromString {}
+extension Int64: IntegerFromString {}
+extension UInt64: IntegerFromString {}
+
+func readInteger<I>() -> I? where I: IntegerFromString {
+    guard let inputString = readLine(strippingNewline: true) else {
+        print("Failed to read string from input")
+        return nil
+    }
+    
+    guard let integer = I.init(inputString) else {
+        print("Failed to read integer from input")
+        return nil
+    }
+    
+    return integer
+}
